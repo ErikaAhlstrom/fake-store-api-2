e58f4edc15386250f1a8f2ee66fe6be4db543978
“erikaahlstrom” <info@erikasoldh.se>
2021-05-04 20:34:25 +0200

    Clean up and add comments.

--- Products.php
+++ Products.php
@@ -11,26 +11,6 @@ class Products {
                 
         // Kolla att query string är valid
         self::checkQuery();
-        
-        // $filteredDataCategory = array();
-        // $filteredDataLimit = array();
-        
-        
-   /*      if (!self::$limit && self::$category) {
-            foreach (self::$data as $product) {
-                if ($product['category'] == self::$category) {
-                    array_push($filteredDataCategory, $product);
-                }
-            }
-            echo json_encode($filteredDataCategory, JSON_UNESCAPED_UNICODE);
-        } */
-/*         if (self::$limit && !self::$category) {
-            shuffle(self::$data);
-            for($i = 0; $i < self::$limit; $i++) {
-                array_push($filteredDataLimit, self::$data[$i]);
-            }
-            echo json_encode($filteredDataLimit, JSON_UNESCAPED_UNICODE);
-        } */
 
     }
 
@@ -48,11 +28,12 @@ class Products {
         }
 
         // Skicka error om fler än 2 queries
-        if(count($_GET) > 2) {
+        if (count($_GET) > 2) {
             $error = "Only 'show' and 'category' filters are allowed.";
             echo json_encode($error, JSON_UNESCAPED_UNICODE);
             die();
         }
+
         // Skicka error om query key är ogiltig
         if (count($_GET) == 2 &&
             !array_key_exists('show', $_GET) &&
@@ -60,17 +41,19 @@ class Products {
             $error = "The key/keyes are not valid.";
             echo json_encode($error, JSON_UNESCAPED_UNICODE);
             die();
-        } else if(count($_GET) == 2 &&
-            array_key_exists('show', $_GET) &&
-            array_key_exists('category', $_GET)
-        ) {
+        } 
+
+        // Körs om både category och show finns i query
+        if (count($_GET) == 2 &&
+        array_key_exists('show', $_GET) &&
+        array_key_exists('category', $_GET)) {
             self::setLimit();
             self::setCategory();
             self::renderLimitCategory();
         }
 
         if (count($_GET) == 1) {
-            // echo "1 query";
+
             if (array_key_exists('show', $_GET)) {
                 self::setLimit();
                 self::renderLimit();
@@ -87,6 +70,7 @@ class Products {
         }
     }
 
+    // Renderar ut data med show filter
     public static function renderLimit() {
         
         $filteredDataLimit = array();
@@ -99,6 +83,7 @@ class Products {
         echo json_encode($filteredDataLimit, JSON_UNESCAPED_UNICODE);
     }
 
+    // Renderar ut data med categoryfilter
     public static function renderCategory() {
         $filteredDataCategory = array();
 
@@ -110,6 +95,7 @@ class Products {
         echo json_encode($filteredDataCategory, JSON_UNESCAPED_UNICODE);
     }
 
+    // Renderar ut data med både show och category filter
     public static function renderLimitCategory() {
         $filteredDataCategory = array();
         $filteredDataLimit = array();
@@ -134,31 +120,32 @@ class Products {
         echo json_encode($filteredDataLimit, JSON_UNESCAPED_UNICODE);
     }
 
+    // felsöker och sätter inkommande show query
     public static function setLimit() {
         $limit = htmlspecialchars($_GET['show']);
 
         // Skicka error om value är större än 20
-        if(self::$limit > 20) {
+        if (self::$limit > 20) {
             $error = "Can't show more than 20 products.";
             echo json_encode($error, JSON_UNESCAPED_UNICODE);
             die();
         }
 
         // Skicka error om show value inte är en siffra
-        if(!is_numeric($limit)) {
+        if (!is_numeric($limit)) {
             $error = "Value must be a number.";
             echo json_encode($error, JSON_UNESCAPED_UNICODE);
             die();
         }
         self::$limit = $limit;
     }
-
+    // felsöker och sätter inkommande category query
     public static function setCategory() {
         // $category = isset($_GET['category']) ? $_GET['category'] : null;
         $category = htmlspecialchars($_GET['category']);
 
         // Skicka error om category är ogiltig
-        if( $category !== "men" &&
+        if ($category !== "men" &&
             $category !== "jewelery" &&
             $category !== "electronics" &&
             $category !== "women") 
